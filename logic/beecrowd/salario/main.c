ca91d999dcee521f85ece2d3c67fa4c71c502b56
Cattozzi <112444201+Cattozzi@users.noreply.github.com>
2026-07-02 18:15:47 -0300

    Add files via upload

---
+++ logic/beecrowd/salario/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int horasTrabalhadas,numero;
+    double valorHora,salario;
+
+    scanf("%d",&numero);
+    scanf("%d",&horasTrabalhadas);
+    scanf("%lf",&valorHora);
+    salario = horasTrabalhadas * valorHora;
+
+    printf("NUMBER = %d\n",numero);
+    printf("SALARY = U$ %.2lf\n",salario);
+    return 0;
+}
