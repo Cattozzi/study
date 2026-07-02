c77237f7c27834b0aa5b7eafae7bb54cba68cd0e
Cattozzi <112444201+Cattozzi@users.noreply.github.com>
2026-07-02 17:37:06 -0300

    Add files via upload

---
+++ logic/beecrowd/average2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    double a, b, c, media;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    media = ((a*2) + (b*3) + (c*5)) / 10;
+    printf("MEDIA = %.1f\n", media);
+    return 0;
+}
