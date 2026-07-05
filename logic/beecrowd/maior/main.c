d5531c7c857dd3f349d5e492a7cd8e18f4fd6bba
Cattozzi <112444201+Cattozzi@users.noreply.github.com>
2026-07-05 19:49:21 -0300

    Add files via upload

---
+++ logic/beecrowd/maior/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+    int a,b,c;
+    int maiorAB;
+
+    scanf("%d%d%d",&a,&b,&c);
+
+    maiorAB=(a+b+abs(a-b))/2;
+
+    int maiorFinal=(maiorAB+c+abs(maiorAB-c))/2;
+
+    printf("%d eh o maior\n",maiorFinal);
+    return 0;
+}
